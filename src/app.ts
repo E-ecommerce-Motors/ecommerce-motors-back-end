bf874a598209721a094965c2532e241751180641
Tierry <tierryhahn@gmail.com>
2023-02-22 15:14:44 -0300

    fix/configs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import "express-async-errors";
+import "reflect-metadata";
 import express from 'express';
 import { appRoutes } from "./routes";
 
@@ -6,4 +8,3 @@ export const app = express();
 app.use(express.json())
 
 appRoutes(app)
-
