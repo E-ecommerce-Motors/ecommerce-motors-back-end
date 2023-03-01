5c7b5ad2edff4fb0db5486f0a7704ef482397e1d
vinMoreiraHenrique <viniciusmoreirahenrique@gmail.com>
2023-03-01 18:32:43 -0300

    feat: editUserRoute created and already have some error tratactives

--- prisma/migrations/20230224151606_change_price_to_float/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Announcement" ALTER COLUMN "price" SET DATA TYPE DOUBLE PRECISION;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,13 +1,22 @@
 import { Router } from "express";
 import { createUserController } from "../controllers/user/createuser.controller";
+import { updateUserController } from "../controllers/user/updateUser.controller";
+import { idExist } from "../middlewares/ensure.middleware";
+import { handleErrorMiddleware } from "../middlewares/handleError.middleware";
 import { validateSchemaMiddleware } from "../middlewares/validateSchema.middleware";
+import { verifyAuthUserMiddleware } from "../middlewares/verifyAuthUser.middleware";
 import { createSchema } from "../schema/user";
 
 const routes = Router();
 
 export const userRoutes = () => {
-    routes.post("", createUserController);
+  routes.post("", createUserController);
+  routes.patch(
+    "/:id",
+    verifyAuthUserMiddleware,
+    updateUserController,
+    handleErrorMiddleware
+  );
 
-
-    return routes;
+  return routes;
 };
