7bd9eb76378f97d619cc5009aa4781550a9f36bf
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-03-03 15:38:53 -0300

    fix: Indentation

--- src/controllers/announcements/createAnnouncement.controller.ts
+++ src/controllers/announcements/createAnnouncement.controller.ts
@@ -4,6 +4,7 @@ import { AnnouncementCreateInput } from '../../interfaces/announcement';
 
 export const createAnnouncementController = async (req: Request, res: Response) => {
   const data: AnnouncementCreateInput = req.body;
+  
   const newAnnouncement = await createAnnouncementService(data);
   
   return res.status(201).json(newAnnouncement);
--- src/controllers/comments/createComments.controller.ts
+++ src/controllers/comments/createComments.controller.ts
@@ -3,5 +3,6 @@ import { createComment } from "../../services/comments/createComments.service";
 
 export const createCommentController = async (req: Request, res: Response) => {
   const comment = await createComment(req.body, Number(req.params.id));
+  
   return res.status(201).json(comment);
 };
