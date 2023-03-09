ccada35126c6a4277607910600712cdd17c6cc48
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-03-09 13:30:16 -0300

    feat: User include address

--- src/controllers/comments/listComment.controller.ts
+++ src/controllers/comments/listComment.controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
-import { listComment } from "../../services/comments/listComments.service";
+import { listCommentService } from "../../services/comments/listComments.service";
 
 export const listCommentController = async (req: Request, res: Response) => {
-  const comments = await listComment(Number(req.params.id));
-  
+  const comments = await listCommentService(Number(req.params.id));
+
   return res.status(201).json(comments);
 };
