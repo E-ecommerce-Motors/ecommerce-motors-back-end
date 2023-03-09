f2160f8f41c196f6ce6b61627743054e1b18c8c9
vinMoreiraHenrique <viniciusmoreirahenrique@gmail.com>
2023-03-09 11:05:27 -0300

    pull

--- src/services/comments/editComment.service.ts
+++ src/services/comments/editComment.service.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { EditComment } from "../../interfaces/announcement";
+import { AppError } from "../../errors/appError";
 
 const prisma = new PrismaClient();
 
 export const editCommentService = async (
   data: EditComment,
-  id: number,
-  commentId: number
+  commentId: number,
+  announcementId: number,
+  id: number
 ) => {
+
+  const findComment = await prisma.address.findUnique({
+    where: {
+            id
+    },
+})
+
+if (!findComment) {
+  throw new AppError("comment", 404)
+}
+
   const comment = await prisma.comment.update({
     where: {
       id: commentId,
