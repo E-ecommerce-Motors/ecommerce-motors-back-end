982e7bc279df57b2cd3328f8356b77e0ca675444
Galvs135 <lg_franca@hotmail.com>
2023-03-09 13:45:45 -0300

    fix: added include address

--- src/services/user/listYourself.service.ts
+++ src/services/user/listYourself.service.ts
@@ -1,12 +1,10 @@
-import { prisma } from "../../utils/prisma"
+import { prisma } from "../../utils/prisma";
 
 export const listYourselfService = async (id: number) => {
-
-
   const user = await prisma.user.findUnique({
-    where: { id }
-  })
+    where: { id },
+    include: { Address: true },
+  });
 
-  return user
-}
-
+  return user;
+};
